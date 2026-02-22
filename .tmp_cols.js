b7bab8d1d17904f8592d1f8aa76157a28a690eac
Ajay Shah(Personal) <realtutorialh@gmail.com>
2026-02-23 04:22:13 +0530

    Cleanup temporary files and sync state

--- .tmp_cols.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { Client } = require('pg');
-(async () => {
-  const c = new Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
-  await c.connect();
-  const res = await c.query("SELECT column_name FROM information_schema.columns WHERE table_name='exam_questions'");
-  console.log(res.rows.map(r => r.column_name));
-  await c.end();
-})();
